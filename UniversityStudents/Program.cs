dc482fe7da4611706e627abc0cc903b89d1a105b
fellly <fellyka@gmail.com>
2023-06-12 10:21:34 +0200

    CORS configuration extension method added

--- UniversityStudents/Program.cs
+++ UniversityStudents/Program.cs
@@ -1,3 +1,11 @@
+/*
+  The builder variable of the type WebApplicationBuilder is created
+  The WebApplicationBuilder is responsible of 4 main things:
+    - Adding Configuration to the project by using the builder.Configuration property
+    - Registering services with the builder.Services property
+    - Logging configuration with the buider.Logging property
+    - Other IHostBuilder and IWebHostBuilder configuration
+ */
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
