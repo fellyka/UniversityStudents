af2950706d4629f7a369b7dc66e49f4330ff0dbc
fellly <fellyka@gmail.com>
2023-06-19 23:12:05 +0200

    Models classes

---
+++ Entities/Models/Faculty.cs
@@ -0,0 +1,22 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    public class Faculty
+    {
+        [Column("FacultyId")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Faculty name is a required field.")]
+        [MaxLength(30, ErrorMessage ="Maximum length for faculty name is 30 characters.")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Faculty building's name is a required field.")]
+        [MaxLength(30, ErrorMessage = "Maximum length for faculty building's name is 30 characters.")]
+        public string? BuildingName { get; set; }
+
+        public ICollection<Student>? Students { get; set; }
+    }
+}
---
+++ Entities/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    public class Student
+    {
+        [Column("StudentId")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Student name is a required field")]
+        [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Age is a required field")]
+        public int Age { get; set; }
+
+        // Navigational properties
+        [ForeignKey(nameof(Faculty))]
+        public Guid FacultyId { get; set; }
+        public Faculty? Faculty { get; set; }
+
+
+    }
+}
